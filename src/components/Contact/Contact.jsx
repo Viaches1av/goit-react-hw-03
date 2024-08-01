a68eb0b8dace378467b08b2973b30a513d7ae145
Viacheslav <slavavilhov@gmail.com>
2024-08-01 20:29:09 +0200

    add

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import styles from './Contact.module.css';
+
+const Contact = ({ contact, deleteContact }) => {
+  return (
+    <li className={styles.container}>
+      <div className={styles.info}>
+        <span className={styles.name}>{contact.name}</span>
+        <span className={styles.number}>{contact.number}</span>
+      </div>
+      <button onClick={() => deleteContact(contact.id)} className={styles.button}>Delete</button>
+    </li>
+  );
+};
+
+export default Contact;
